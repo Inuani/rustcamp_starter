ded162fe5cd30b412f1f2db337e97b194c0ede99
Elie <100168298+Inuani@users.noreply.github.com>
2023-11-14 11:34:13 +0000

    doing lvl1 bootcamp

--- src/rust_canister/src/lib.rs
+++ src/rust_canister/src/lib.rs
@@ -1,13 +1,31 @@
-use ic_cdk::{query, export_candid};
+use ic_cdk::{query, update, export_candid};
+use std::cell::RefCell;
 
-#[query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+thread_local!{
+
+    //This is a string literal. In Rust, string literals have a 'static lifetime because they are embedded directly in the final binary and are always available during the program execution.
+    pub static NAME: &'static str = "AlienChainDao";
+
+    //In Rust, static variables are global and remain in memory for the lifetime of the running program.
+    // Static variables in Rust are immutable by default. If you need to modify a static variable, you must use a type that supports interior mutability, like RefCell or Mutex.
+    // RefCell doesn't provide thread safety
+    // RefCell provides "interior mutability".  even if RefCell is accessed through an immutable reference, you can still change the data inside it. 
+    pub static MANIFESTO: RefCell<String> = RefCell::new("Go look for aliens in the ICPverse".to_string());
 }
 
 #[query]
-fn greet_two_times(name: String) -> String {
-    format!("Hello, {}! Hello {}!", name, name)
+fn get_name() -> &'static str {
+    NAME.with(|name| *name)
+}
+
+#[query]
+fn get_manifesto() -> String {
+    MANIFESTO.with(|manifesto| manifesto.borrow().clone())
+}
+
+#[update]
+fn set_manifesto(new_manifesto: String) {
+    MANIFESTO.with(|manifesto| *manifesto.borrow_mut() = new_manifesto);
 }
 
 export_candid!();
